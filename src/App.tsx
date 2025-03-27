1c99e26a1d5a875e83d8752327fc1946d8adbacd
Burak Adıgüzel <adiguzelburak@icloud.com>
2025-03-27 12:13:27 +0300

    build: update vite version for tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 export function App() {
   return (
-    <div className="text-red-500">
-      <h1>Hello World</h1>
+    <div className="max-w-[1440px] mx-auto flex flex-col items-center justify-center h-screen">
+      <div className="h-[736px] bg-red-400 w-full">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold">Hello World</h1>
+        </div>
+      </div>
     </div>
   );
 }
